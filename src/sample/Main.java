6bc7e5d423ac012cc2b8cd265025e8222bf73f90
System Administrator <root@MacBook-Pro-de-Clement.local>
2016-11-15 16:20:35 +0100

    version eclipse

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,14 +7,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
         primaryStage.setTitle("Take Care - Cardium");
-        primaryStage.setScene(new Scene(root, 1280, 720, Color.GREY));
+        primaryStage.setScene(new Scene(root, 1280, 720, Color.YELLOW));
         primaryStage.show();
 
     }
